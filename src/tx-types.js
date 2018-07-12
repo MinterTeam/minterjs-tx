8d83fdcc67cdc299bd1ccbe7fa4f372dbdc08288
shrpne <shrpne@gmail.com>
2018-07-12 21:31:11 +0300

    update tx types

--- src/tx-types.js
+++ src/tx-types.js
@@ -1,8 +1,10 @@
 export const TX_TYPE_SEND = '0x01';
-export const TX_TYPE_EXCHANGE = '0x02';
-export const TX_TYPE_CREATE_COIN = '0x03';
-export const TX_TYPE_DECLARE_CANDIDACY = '0x04';
-export const TX_TYPE_DELEGATE = '0x05';
-export const TX_TYPE_REDEEM_CHECK = '0x07';
-export const TX_TYPE_SET_CANDIDATE_ON = '0x08';
-export const TX_TYPE_SET_CANDIDATE_OFF = '0x09';
+export const TX_TYPE_SELL_COIN = '0x02';
+export const TX_TYPE_BUY_COIN = '0x03';
+export const TX_TYPE_CREATE_COIN = '0x04';
+export const TX_TYPE_DECLARE_CANDIDACY = '0x05';
+export const TX_TYPE_DELEGATE = '0x06';
+export const TX_TYPE_UNBOUND = '0x07';
+export const TX_TYPE_REDEEM_CHECK = '0x08';
+export const TX_TYPE_SET_CANDIDATE_ON = '0x09';
+export const TX_TYPE_SET_CANDIDATE_OFF = '0x0a';
