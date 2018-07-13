a2370552735a607c58498420520430ce6c2a6f25
shrpne <shrpne@gmail.com>
2018-07-13 13:01:16 +0300

    new tx data

---
+++ src/tx-data/sellCoin.js
@@ -0,0 +1,41 @@
+import ethUtil from 'ethereumjs-util';
+import {Buffer} from 'safe-buffer';
+
+class SellCoinTransactionData {
+    constructor(data) {
+        data = data || {};
+        // Define Properties
+        const fields = [
+            {
+                name: 'coin_to_sell',
+                allowZero: true,
+                length: 10,
+                default: new Buffer([]),
+            },
+            {
+                name: 'value_to_sell',
+                length: 32,
+                allowZero: true,
+                allowLess: true,
+                default: new Buffer([]),
+            },
+            {
+                name: 'coin_to_buy',
+                allowZero: true,
+                length: 10,
+                default: new Buffer([]),
+            }];
+
+        /**
+         * Returns the rlp encoding of the transaction
+         * @method serialize
+         * @return {Buffer}
+         * @memberof Transaction
+         * @name serialize
+         */
+        // attached serialize
+        ethUtil.defineProperties(this, fields, data);
+    }
+}
+
+export default SellCoinTransactionData;
